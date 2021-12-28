28b9f81a86d6b8f5bdd84ea13f8f734887872023
HDU-Nbsp <83355040+HDU-Nbsp@users.noreply.github.com>
2021-12-28 15:42:33 +0800

    Add files via upload

--- assets/js/gitStar.js
+++ assets/js/gitStar.js
@@ -1,6 +1,4 @@
 //打开项目时的入口函数
 function openProject (event) {
-  let hash = parent.window.location.hash
-  parent.window.location.hash = hash + '?' + "projectName=" + event.target.textContent
   window.parent.giveMark(event.target.parentElement.children[0].textContent)
 }
