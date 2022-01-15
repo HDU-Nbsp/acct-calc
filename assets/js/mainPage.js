fcb92e3abb41d2d48f6eb5c243536c95fe70f3a2
HDU-Nbsp <83355040+HDU-Nbsp@users.noreply.github.com>
2022-01-15 09:18:22 +0800

    Add files via upload

--- assets/js/mainPage.js
+++ assets/js/mainPage.js
@@ -1,12 +1,19 @@
 let i = 1;
 
 function changeIMG() {
+  $('img').fadeIn(500);
   i++
   document.querySelector('img').src ='../assets/img/' + i + '.png'
   if (i == 3) {
     i = 0
   }
-  setTimeout(changeIMG,3000)
+  setTimeout(function() {
+    $('img').fadeOut(500);
+  },3500)
+  setTimeout(changeIMG,4000)
 }
 
-setTimeout(changeIMG,3000)
+setTimeout(function() {
+  $('img').fadeOut(500);
+},3500)
+setTimeout(changeIMG,4000)
